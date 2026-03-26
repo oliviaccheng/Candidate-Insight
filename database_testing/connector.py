34ecb11723d288e8801e46391c4b1fae004bb39c
wdouglass078 <williamdouglass078@gmail.com>
2026-03-26 09:39:35 -0400

    added read_candidate func, working on create_candidate

--- database_testing/connector.py
+++ database_testing/connector.py
@@ -1,26 +1,43 @@
 import psycopg2
 import getpass
-# this can be made more portable by creating a database.ini file 
-# and reading the connection params from there using a configparser.
-# this is for simplicity, but can be implemented later (like mid-late April)
-p = getpass.getpass("Password: ")
-connection = psycopg2.connect(host="localhost", port=5432,database="les_candidats", user="postgres", password=p) 
 
-crsr = connection.cursor()
-print("PostgreSQL database version: ")
-# crsr can be used to execute SQL queries. we use it here to grab the version.
+def main():
+    # this can be made more portable by creating a database.ini file 
+    # and reading the connection params from there using a configparser.
+    # this is for simplicity, but can be implemented later (like mid-late April)
+    p = getpass.getpass("Password: ")
+    connection = psycopg2.connect(host="localhost", port=5432,database="les_candidats", user="postgres", password=p) 
 
-crsr.execute("SELECT version()")
-db_version = crsr.fetchone()
-print(db_version)
+    crsr = connection.cursor()
+    print("PostgreSQL database version: ")
+    # crsr can be used to execute SQL queries. we use it here to grab the version.
 
-# also, the name of the table is candidates, which is distinctly english.
-crsr.execute("SELECT * FROM candidates")
-rows = crsr.fetchall()
-#rows = crsr.fetchmany(5)
-for row in rows:   
-    print(row)
+    crsr.execute("SELECT version()")
+    db_version = crsr.fetchone()
+    print(db_version)
 
-# other notes:
-# fetchone = one line, fetchall = all lines, fetchmany = # of lines.
+    # also, the name of the table is candidates, which is distinctly english.
+    crsr.execute("SELECT * FROM candidates")
+    rows = crsr.fetchall()
+    #rows = crsr.fetchmany(5)
+    # each row is a tuple.
+    for row in rows:   
+        print(row)
 
+    # other notes:
+    # fetchone = one line, fetchall = all lines, fetchmany = # of lines.
+    read_candidate("Matt Van Epps", crsr)
+
+# we don't want people injecting whatever, so here are functions to control database access.
+#def create_candidate():
+    
+def read_candidate(name, crsr):
+    crsr.execute("SELECT * FROM candidates WHERE name = %s", (name,))
+    print(crsr.fetchone())
+    
+#def update_candidate():
+    
+    
+
+if __name__ == "__main__":
+    main()
